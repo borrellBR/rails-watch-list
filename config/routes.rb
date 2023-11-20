ad39831fe0f8df237fbf166691ac200d8cf82b21
Ignacio Borrell <ignacioborrellr@gmail.com>
2023-11-20 10:09:38 +0100

    x

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,4 @@ Rails.application.routes.draw do
   root to: 'movies#index'
 
   resources :movies, only: [:index, :show, :new, :create, :destroy]
-  resources :lists do
-    resources :bookmarks, only: [:new, :create]
-  end
-  resources :bookmarks, only: [:destroy]
 end
